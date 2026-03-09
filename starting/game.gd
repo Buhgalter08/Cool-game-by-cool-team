f4d8feea0af2a955eaded9cea4a1fd185814729f
Buhgalter08 <twinkaccbuch@gmail.com>
2026-03-09 11:08:59 +0200

    Add files via upload

--- starting/game.gd
+++ starting/game.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+var coinsCount=0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,6 +12,8 @@ func _process(delta: float) -> void:
 	
 func _input(event: InputEvent) -> void:
 	#print(event)
+	#if($Icon.position==$Coin.position):
+		#$Coin.visible = false
 	if event is InputEventKey:
 		if event.pressed:
 			if event.keycode==65:
@@ -25,3 +27,15 @@ func _input(event: InputEvent) -> void:
 		if event.pressed:
 			if event.keycode==83:
 				$Icon.position.y+=10
+				
+func _on_area_entered(area: Area2D) -> void:
+	area.queue_free()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	coinsCount+=1;
+	area.queue_free()
+	$Label.text="Coins:"+str(coinsCount);
+	
+	
+	
